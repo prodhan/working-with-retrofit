8015b289903b588e224884cd427fde6b49ee8f11
Ariful Islam <arifulweb007@gmail.com>
2019-01-10 01:30:54 +0600

    Cnfigure Retrofit

---
+++ app/src/main/java/com/pigeonsoft/retrofitexample/Interface/GetDataService.java
@@ -0,0 +1,14 @@
+package com.pigeonsoft.retrofitexample.Interface;
+
+import com.pigeonsoft.retrofitexample.Model.RetroPhoto;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetDataService {
+
+    @GET("/photos")
+    Call<List<RetroPhoto>>getAllPhotos();
+}
